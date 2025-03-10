7aa6d2a0f35e23d8320e902d30d71a55e7cb46a8
Gorm42 <161018775+Gorm42@users.noreply.github.com>
2025-03-10 09:48:33 +0100

    TCP server oprettet.

---
+++ MandatoryAssignmentServer/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MandatoryAssignmentServer;
+
+Console.WriteLine("Hello, World!");
+
+Server server = new Server();
+server.Start();
+
+Console.ReadKey();
---
+++ MandatoryAssignmentServer/Server.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MandatoryAssignmentServer
+{
+    public class Server
+    {
+        private const int PORTNUMMER = 21;
+        public void Start()
+        {
+            TcpListener server = new TcpListener(PORTNUMMER);
+            server.Start();
+            Console.WriteLine("Server startet at port" + PORTNUMMER);
+
+            TcpClient socket = server.AcceptTcpClient(); //Svarer til en socket. Venter på 3-way handskake.
+
+            StreamReader reader = new StreamReader(socket.GetStream()); //Læser fra socket
+            StreamWriter writer = new StreamWriter(socket.GetStream()); //Skriver til socket
+            //Nu kan vi læse og skrive string.
+            writer.AutoFlush = true;
+
+            //Her begynder protokollen
+
+
+
+
+            string? line = reader.ReadLine();
+            Console.WriteLine("Line from client is: " + line);
+
+            line = line.ToUpper();
+
+            writer.WriteLine(line);
+            writer.Flush(); //Flusher bufferen
+
+            socket?.Close();             
+        }
+
+    }
+}

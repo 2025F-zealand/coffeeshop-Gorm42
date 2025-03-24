3a382497039498bcc22502ac161b1b99d4d3575e
Gorm42 <161018775+Gorm42@users.noreply.github.com>
2025-03-24 15:25:54 +0100

    Test af om det virker.

--- NyAktieHandelRepoLibTests/AktieHandelRepositoryTests.cs
+++ NyAktieHandelRepoLibTests/AktieHandelRepositoryTests.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace NyAktieHandelRepoLib.Tests
 {
-    [TestClass()]
+    [TestClass()] //
     public class AktieHandelRepositoryTests
     {
         [TestMethod()]
